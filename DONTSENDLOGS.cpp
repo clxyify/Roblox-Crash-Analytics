7f6670111dc66ccce6e5f949650b5bb8604c1100
electrodev <98536033+clxyify@users.noreply.github.com>
2022-05-14 14:32:26 -0400

    Create DONTSENDLOGS.cpp

---
+++ DONTSENDLOGS.cpp
@@ -0,0 +1,412 @@
+int __thiscall sub_694300(_DWORD **this)//LOL '69' anyways __this__ is a dword.
+{
+  int v2; // eax
+  int v3; // esi
+  int v4; // eax
+  int v5; // esi
+  int v6; // eax
+  __int64 v7; // rax
+  int v8; // ecx
+  int v9; // eax
+  int v10; // ecx
+  int v11; // ecx
+  const char *v12; // eax
+  int *v13; // eax
+  int v14; // ecx
+  int *v15; // eax
+  int v16; // ecx
+  unsigned int v17; // edi
+  int v18; // esi
+  int *v19; // eax
+  _DWORD *v20; // eax
+  int v21; // eax
+  int v22; // esi
+  int *v23; // eax
+  int v24; // edi
+  char *v25; // eax
+  int v26; // eax
+  int v28; // [esp-18h] [ebp-5D8h]
+  int v29; // [esp-4h] [ebp-5C4h]
+  int v30; // [esp+0h] [ebp-5C0h] BYREF
+  char v31[16]; // [esp+Ch] [ebp-5B4h] BYREF
+  char v32[12]; // [esp+BCh] [ebp-504h] BYREF
+  int v33; // [esp+C8h] [ebp-4F8h]
+  char v34[12]; // [esp+E4h] [ebp-4DCh] BYREF
+  unsigned int v35; // [esp+F0h] [ebp-4D0h]
+  char v36[16]; // [esp+10Ch] [ebp-4B4h] BYREF
+  int v37; // [esp+11Ch] [ebp-4A4h]
+  int v38; // [esp+120h] [ebp-4A0h]
+  char v39[8]; // [esp+13Ch] [ebp-484h] BYREF
+  int v40; // [esp+14Ch] [ebp-474h]
+  int v41; // [esp+150h] [ebp-470h]
+  char v42[12]; // [esp+16Ch] [ebp-454h] BYREF
+  int v43; // [esp+17Ch] [ebp-444h]
+  int v44; // [esp+180h] [ebp-440h]
+  __int128 v45; // [esp+184h] [ebp-43Ch]
+  __int64 v46; // [esp+194h] [ebp-42Ch]
+  char v47[12]; // [esp+19Ch] [ebp-424h] BYREF
+  int v48; // [esp+1ACh] [ebp-414h]
+  int v49; // [esp+1B0h] [ebp-410h]
+  int v50[6]; // [esp+1CCh] [ebp-3F4h] BYREF
+  int v51[6]; // [esp+1FCh] [ebp-3C4h] BYREF
+  int v52[6]; // [esp+28Ch] [ebp-334h] BYREF
+  int v53[6]; // [esp+2BCh] [ebp-304h] BYREF
+  char v54[32]; // [esp+2ECh] [ebp-2D4h] BYREF
+  unsigned int v55; // [esp+320h] [ebp-2A0h]
+  int v56[6]; // [esp+324h] [ebp-29Ch]
+  int v57[2]; // [esp+33Ch] [ebp-284h] BYREF
+  char v58[44]; // [esp+344h] [ebp-27Ch] BYREF
+  int v59; // [esp+370h] [ebp-250h]
+  int v60; // [esp+374h] [ebp-24Ch]
+  int v61; // [esp+378h] [ebp-248h]
+  int v62[18]; // [esp+38Ch] [ebp-234h] BYREF
+  char v63[80]; // [esp+3D4h] [ebp-1ECh] BYREF
+  int v64; // [esp+424h] [ebp-19Ch]
+  unsigned int v65; // [esp+428h] [ebp-198h]
+  int v66[5]; // [esp+42Ch] [ebp-194h] BYREF
+  unsigned int v67; // [esp+440h] [ebp-180h]
+  int v68[5]; // [esp+444h] [ebp-17Ch] BYREF
+  unsigned int v69; // [esp+458h] [ebp-168h]
+  int v70[5]; // [esp+45Ch] [ebp-164h] BYREF
+  unsigned int v71; // [esp+470h] [ebp-150h]
+  int v72[5]; // [esp+474h] [ebp-14Ch] BYREF
+  unsigned int v73; // [esp+488h] [ebp-138h]
+  int v74[5]; // [esp+48Ch] [ebp-134h] BYREF
+  unsigned int v75; // [esp+4A0h] [ebp-120h]
+  int v76[5]; // [esp+4A4h] [ebp-11Ch] BYREF
+  unsigned int v77; // [esp+4B8h] [ebp-108h]
+  int v78[5]; // [esp+4C4h] [ebp-FCh] BYREF
+  unsigned int v79; // [esp+4D8h] [ebp-E8h]
+  int v80[5]; // [esp+4DCh] [ebp-E4h] BYREF
+  unsigned int v81; // [esp+4F0h] [ebp-D0h]
+  int v82[5]; // [esp+4F4h] [ebp-CCh] BYREF
+  unsigned int v83; // [esp+508h] [ebp-B8h]
+  int v84[5]; // [esp+50Ch] [ebp-B4h] BYREF
+  unsigned int v85; // [esp+520h] [ebp-A0h]
+  int v86[5]; // [esp+524h] [ebp-9Ch] BYREF
+  unsigned int v87; // [esp+538h] [ebp-88h]
+  char v88[16]; // [esp+53Ch] [ebp-84h] BYREF
+  int v89; // [esp+54Ch] [ebp-74h]
+  unsigned int v90; // [esp+550h] [ebp-70h]
+  int v91[4]; // [esp+554h] [ebp-6Ch] BYREF
+  int v92; // [esp+564h] [ebp-5Ch]
+  unsigned int v93; // [esp+568h] [ebp-58h]
+  int v94[4]; // [esp+56Ch] [ebp-54h] BYREF
+  int v95; // [esp+57Ch] [ebp-44h]
+  unsigned int v96; // [esp+580h] [ebp-40h]
+  int v97[4]; // [esp+584h] [ebp-3Ch] BYREF
+  int v98; // [esp+594h] [ebp-2Ch]
+  unsigned int v99; // [esp+598h] [ebp-28h]
+  _DWORD **v100; // [esp+59Ch] [ebp-24h]
+  int v101; // [esp+5A0h] [ebp-20h] BYREF
+  double v102; // [esp+5A4h] [ebp-1Ch]
+  int v103; // [esp+5ACh] [ebp-14h]
+  int *v104; // [esp+5B0h] [ebp-10h]
+  int v105; // [esp+5BCh] [ebp-4h]
+
+  v103 = 0;
+  v105 = 1;
+  strcpy(v36, "userId");//DONT LOG MY USER ID
+  v104 = &v30;
+  v100 = this;
+  v38 = 15;
+  v37 = 6;
+  sub_663A00(a113905901);
+  strcpy(v39, "placeId");//OH NO DONT LOG MY PLACE ID!
+  v41 = 15;
+  v40 = 7;
+  LOBYTE(v105) = 3;
+  sub_663A00(a12109643);
+  LOBYTE(v105) = 4;
+  v2 = sub_1AB6450(v68, "%d", 2);
+  LODWORD(v45) = 0;
+  v46 = 0i64;
+  v44 = 15;
+  v43 = 10;
+  strcpy(v42, "platformId");//DONT LOG MY PLATFORM ID
+  v45 = *(_OWORD *)v2;
+  v46 = *(_QWORD *)(v2 + 16);
+  *(_DWORD *)(v2 + 16) = 0;
+  *(_DWORD *)(v2 + 20) = 15;
+  *(_BYTE *)v2 = 0;
+  strcpy(v88, "Win32");//DONT CHECK MY OS
+  v90 = 15;
+  v89 = 5;
+  v103 = 1;
+  v49 = 15;
+  v48 = 10;
+  strcpy(v47, "osPlatform");//DONT CHECK MY OS
+  sub_6635D0(v88);
+  LOBYTE(v105) = 8;
+  v3 = sub_1271D00(v70);
+  LOBYTE(v105) = 9;
+  v50[0] = 0;
+  v50[4] = 0;
+  v50[5] = 15;
+  sub_663D40(v50, (int)"osVersion", 9u);//DONT CHECK MY OS
+  sub_6635D0(v3);
+  LOBYTE(v105) = 10;
+  v51[0] = 0;
+  v51[4] = 0;
+  v51[5] = 15;
+  sub_663D40(v51, (int)"appVersion", 0xAu);//DONT CHECK MY VERSION
+  LOBYTE(v105) = 11;
+  sub_663A00(a052605260426_1);
+  LOBYTE(v105) = 12;
+  v29 = sub_12723A0(v72);
+  LOBYTE(v105) = 13;
+  sub_6982E0("GPU", v29);//DONT CHECK MY GPU
+  LOBYTE(v105) = 14;
+  v4 = sub_12B0190();
+  sub_663A00(v4 + 24);
+  v5 = 3;
+  v103 = 3;
+  LOBYTE(v105) = 15;
+  sub_6982E0("CPU", v78);//DONT CHECK MY CPU
+  LOBYTE(v105) = 16;
+  v6 = MEMORY[0x75EA37B0](v34, 40);
+  MEMORY[0x75EB7E80](v6);
+  v101 = sub_1AB6450(v74, "%llu", (unsigned __int64)v35);
+  LOBYTE(v105) = 17;
+  v52[0] = 0;
+  v52[4] = 0;
+  v52[5] = 15;
+  sub_663D40(v52, (int)"UsedMemory", 0xAu);//DONT CHECK MY USED MEMORY
+  sub_6635D0(v101);
+  LOBYTE(v105) = 18;
+  v7 = sub_129D3C0();
+  v101 = sub_1AB6450(v76, "%llu", v7);
+  LOBYTE(v105) = 19;
+  v53[0] = 0;
+  v53[4] = 0;
+  v53[5] = 15;
+  sub_663D40(v53, (int)"TotalMemory", 0xBu);//DONT CHECK MY total MEMORY
+  sub_6635D0(v101);
+  LOBYTE(v105) = 27;
+  LODWORD(v102) = v36;
+  v101 = 0;
+  sub_6460B0(&v101, v8);
+  LOBYTE(v105) = 28;
+  sub_64B0A0(v36, v54);
+  LOBYTE(v105) = 29;
+  v91[0] = 0;
+  v92 = 0;
+  v93 = 15;
+  sub_663D40(v91, (int)"ClientWindowsCrashAnalytics", 0x1Bu);//DONT CHECK MY uhh idk
+  LOBYTE(v105) = 30;
+  v94[0] = 0;
+  v95 = 0;
+  v96 = 15;
+  sub_663D40(v94, (int)"ClientCrash", 0xBu);//DONT CHECK MY CRASH
+  LOBYTE(v105) = 31;
+  v97[0] = 0;
+  v98 = 0;
+  v99 = 15;
+  sub_663D40(v97, (int)"Client", 6u);//DONT CHECK MY CLIENT
+  LOBYTE(v105) = 32;
+  sub_71F7A0(v91, v54);
+  if ( v99 >= 0x10 )
+    sub_663CC0();
+  v98 = 0;
+  v99 = 15;
+  LOBYTE(v97[0]) = 0;
+  if ( v96 >= 0x10 )
+    sub_663CC0();
+  v95 = 0;
+  v96 = 15;
+  LOBYTE(v94[0]) = 0;
+  if ( v93 >= 0x10 )
+    sub_663CC0();
+  v92 = 0;
+  v93 = 15;
+  LOBYTE(v91[0]) = 0;
+  sub_629670(v54);
+  LOBYTE(v105) = 26;
+  sub_201E0A0(v36, 48, 10, sub_6594C0);
+  if ( v77 >= 0x10 )
+    sub_663CC0();
+  v76[4] = 0;
+  v77 = 15;
+  LOBYTE(v76[0]) = 0;
+  if ( v75 >= 0x10 )
+    sub_663CC0();
+  v74[4] = 0;
+  v75 = 15;
+  LOBYTE(v74[0]) = 0;
+  if ( v79 >= 0x10 )
+    sub_663CC0();
+  v78[4] = 0;
+  v79 = 15;
+  LOBYTE(v78[0]) = 0;
+  if ( v73 >= 0x10 )
+    sub_663CC0();
+  v72[4] = 0;
+  v73 = 15;
+  LOBYTE(v72[0]) = 0;
+  if ( v71 >= 0x10 )
+    sub_663CC0();
+  v70[4] = 0;
+  v71 = 15;
+  LOBYTE(v70[0]) = 0;
+  if ( v90 >= 0x10 )
+    sub_663CC0();
+  v89 = 0;
+  v90 = 15;
+  v88[0] = 0;
+  if ( v69 >= 0x10 )
+    sub_663CC0();
+  v68[4] = 0;
+  v69 = 15;
+  LOBYTE(v68[0]) = 0;
+  v105 = 34;
+  sub_1275A20(-1, 0, 0);
+  LOBYTE(v105) = 35;
+  sub_1277D90("SessionReport", "AppStatusCrash", 0); //DONT CHECK MY IDK
+  v102 = sub_1AA6F40();
+  sub_1277B80("PlayTime");
+  v9 = MEMORY[0x75EA37B0](v32, 40);
+  MEMORY[0x75EB7E80](v9);
+  sub_1277D00("UsedMemoryKB", v33, 0);//DONT CHECK MY KB
+  if ( dword_2E6A3E8 )
+  {
+    v11 = *(_DWORD *)(dword_2E6A3E8 + 44);
+    if ( v11 )
+    {
+      v12 = "inGame";
+      if ( v11 != 1 )
+        v12 = "leaveGame";
+    }
+    else
+    {
+      v12 = "uninitialized";
+    }
+    sub_663E10(v86, v12);
+    LOBYTE(v105) = 36;
+    v13 = v86;
+    if ( v87 >= 0x10 )
+      v13 = (int *)v86[0];
+    sub_1277D90("GameState", v13, 0);//DONT CHECK MY GAME STATE
+    LOBYTE(v105) = 35;
+    if ( v87 >= 0x10 )
+      sub_663CC0();
+    v86[4] = 0;
+    v87 = 15;
+    LOBYTE(v86[0]) = 0;
+  }
+  if ( this )
+  {
+    sub_1277EE0("ExceptionCode", **this, v10);//DONT CHECK MY EXCEPUJNI
+    sub_641E30(v14);
+    LOBYTE(v105) = 37;
+    sub_697950((*this)[3]);
+    sub_645900(v84);
+    v5 = 7;
+    v103 = 7;
+    LOBYTE(v105) = 38;
+    v15 = v84;
+    if ( v85 >= 0x10 )
+      v15 = (int *)v84[0];
+    sub_1277D90("ExceptionAddress", v15, 0);
+    if ( v85 >= 0x10 )
+      sub_663CC0();
+    v84[4] = 0;
+    v17 = 0;
+    v85 = 15;
+    LOBYTE(v84[0]) = 0;
+    while ( v17 < (*v100)[4] )
+    {
+      v56[0] = (int)&unk_227CCE4;
+      v56[4] = (int)&unk_227CCDC;
+      v62[2] = 0;
+      v62[10] = 0;
+      v62[11] = 0;
+      v62[12] = 0;
+      v62[0] = (int)&unk_226B534;
+      LOBYTE(v105) = 39;
+      v18 = v5 | 8;
+      v103 = v18;
+      sub_6456D0(v57, v16);
+      v105 = 40;
+      *(int *)((char *)v56 + *(_DWORD *)(v56[0] + 4)) = (int)&unk_227CCF0;
+      *(unsigned int *)((char *)&v55 + *(_DWORD *)(v56[0] + 4)) = *(_DWORD *)(v56[0] + 4) - 104;
+      sub_647710(v57);
+      v57[0] = (int)&unk_227CC64;
+      v60 = 0;
+      v61 = 0;
+      LOBYTE(v105) = 41;
+      sub_697AB0((*v100)[v17 + 5]);
+      sub_645900(v82);
+      v5 = v18 | 0x10;
+      v103 = v5;
+      LOBYTE(v105) = 42;
+      v19 = v82;
+      if ( v83 >= 0x10 )
+        v19 = (int *)v82[0];
+      HIDWORD(v102) = v19;
+      v20 = (_DWORD *)sub_1AB6450(v66, "ExceptionParameter-%u", v17);
+      LOBYTE(v105) = 43;
+      if ( v20[5] >= 0x10u )
+        v20 = (_DWORD *)*v20;
+      sub_1277D90(v20, HIDWORD(v102), 0);
+      if ( v67 >= 0x10 )
+        sub_663CC0();
+      v66[4] = 0;
+      v67 = 15;
+      LOBYTE(v66[0]) = 0;
+      if ( v83 >= 0x10 )
+        sub_663CC0();
+      v82[4] = 0;
+      v83 = 15;
+      LOBYTE(v82[0]) = 0;
+      *(int *)((char *)v56 + *(_DWORD *)(v56[0] + 4)) = (int)&unk_227CCF0;
+      *(unsigned int *)((char *)&v55 + *(_DWORD *)(v56[0] + 4)) = *(_DWORD *)(v56[0] + 4) - 104;
+      v57[0] = (int)&unk_227CC64;
+      sub_645880(v57);
+      v57[0] = (int)&unk_226B4B4;
+      if ( v59 )
+        sub_643CC0(1);
+      sub_641F30(v58);
+      LOBYTE(v105) = 44;
+      v62[0] = (int)&unk_226B4AC;
+      sub_200A264(v62);
+      ++v17;
+    }
+    LOBYTE(v105) = 35;
+    sub_6296B0(v31);
+  }
+  v21 = sub_699EC0(v10);
+  sub_663A00(v21 + 28);
+  v22 = v5 | 0x20;
+  v103 = v22;
+  LOBYTE(v105) = 45;
+  v23 = v80;
+  if ( v81 >= 0x10 )
+    v23 = (int *)v80[0];
+  sub_1277D90("SessionID", v23, 0);
+  LOBYTE(v105) = 35;
+  if ( v81 >= 0x10 )
+    sub_663CC0();
+  v24 = dword_2CBB048;
+  v80[4] = 0;
+  v81 = 15;
+  LOBYTE(v80[0]) = 0;
+  if ( (unsigned int)(0x7FFFFFFF - dword_2CC4F00) < 0xA )
+    sub_663AD0();
+  v25 = aWindowsclient_2;
+  if ( (unsigned int)dword_2CC4F04 >= 0x10 )
+    v25 = *(char **)aWindowsclient_2;
+  sub_64E3D0(HIDWORD(v102), dword_2CC4F00, v25, dword_2CC4F00, "CrashEvent", 10);
+  v103 = v22 | 0x40;
+  LOBYTE(v105) = 46;
+  v26 = sub_648BD0(&dword_2CC4F08);
+  LOBYTE(v105) = 47;
+  sub_1278690(v26, v24, 0);
+  if ( v65 >= 0x10 )
+    sub_663CC0();
+  v64 = 0;
+  v65 = 15;
+  v63[64] = 0;
+  if ( v55 >= 0x10 )
+    sub_663CC0();
+  return sub_1275B20(v63, v28);
+}
